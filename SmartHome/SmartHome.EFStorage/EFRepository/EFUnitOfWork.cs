45fa19f421bb5b28806a7d3a79ccc0d982afba48
YatsenkoOleksandr <yats.sasha@gmail.com>
2017-01-11 08:04:52 +0200

    Added finilizer to EFUnitOfWork

--- SmartHome/SmartHome.EFStorage/EFRepository/EFUnitOfWork.cs
+++ SmartHome/SmartHome.EFStorage/EFRepository/EFUnitOfWork.cs
@@ -27,37 +27,30 @@ namespace SmartHome.EFStorage.EFRepository
             dbContext.SaveChanges();
         }
 
-        #region IDisposable Support
-        private bool disposedValue = false; // To detect redundant calls
+        
+        private bool disposedValue = false; 
 
         protected virtual void Dispose(bool disposing)
         {
             if (!disposedValue)
             {
                 if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects).
+                {                    
                     dbContext.Dispose();
                 }
 
                 disposedValue = true;
             }
         }
-
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~EFUnitOfWork() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
-
-        // This code added to correctly implement the disposable pattern.
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
+        
+        ~EFUnitOfWork() {           
+            Dispose(false);
         }
-        #endregion
+        
+        public void Dispose()
+        {            
+            Dispose(true);            
+            GC.SuppressFinalize(this);
+        }        
     }
 }
